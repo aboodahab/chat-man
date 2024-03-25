b5ccd0517a85266e46804b7b2bc12a28d7ed9407
Meow <abodkaramma77@gmail.com>
2024-03-25 22:08:58 +0200

    back-end folder

--- back-end/createAccount.js
+++ back-end/createAccount.js
@@ -5,14 +5,13 @@ app.use(express.json());
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const { send } = require("process");
 
 app.use(cors());
 mongoose
   .connect("mongodb://localhost/meo")
   .then(() => console.log("connect to the data base"))
   .catch(() => console.log("field to connect to database"));
-const courseSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -26,7 +25,7 @@ const courseSchema = new mongoose.Schema({
     required: true,
   },
 });
-const m = mongoose.model("Meo", courseSchema);
+const m = mongoose.model("Meo", usersSchema);
 app.post("/signu", async (req, res) => {
   console.log(req.body);
   const checking = await m.find({ email: req.body.email });
@@ -64,14 +63,14 @@ app.post("/check", (req, res) => {
   }
 });
 app.post("/signin", (req, res) => {
- 
   check(req.body)
-    .then(() =>  res.json({ action: "signin" }))
+    .then(() => res.json({ action: "signin" }))
     .catch((e) => {
       console.log("error", e);
       res.json({ action: "Error" });
     });
 });
+
 async function check(data) {
   const account = await m.find({ ...data });
   if (account.length === 0) {
@@ -81,7 +80,50 @@ async function check(data) {
 
   console.log({ success: true });
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------
+mongoose
+  .connect("mongodb://localhost/message")
+  .then(() => console.log("connect to the data base messages"))
+  .catch(() => console.log("field to connect to database messages"));
+const messagesSchema = new mongoose.Schema({
+  msgValue: {
+    type: String,
+    required: true,
+  },
 
+  time: {
+    type: Date,
+    required: true,
+  },
+  string: {
+    type: String,
+    required: true,
+  },
+});
+let msg = mongoose.model("Msg", messagesSchema);
+app.post("/messages", async (req, res) => {
+  console.log(req.body, "req.body");
+  if (req.body.string === null || undefined || 0 || "" || false) {
+    console.log("error");
+    res.status(404).json({ msg: "error" });
+    return;
+  }
+  let time = new Date();
+  let obj = {
+    msgValue: req.body.value,
+    time: time,
+    string: req.body.string,
+  };
+  const message = new msg(obj);
+  await message.save();
+  res.json({ string: req.body.string });
+});
+app.post("/data", async (req, res) => {
+  let findAllMessages = await msg.find().limit(30);
+
+  res.json({ messages: findAllMessages, string: req.body.string });
+});
 app.listen(2000, () => {
   console.log("2000");
 });
+//{}
