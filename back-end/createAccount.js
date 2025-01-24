163608e257fa99c952204c70b87f39c89d29f049
Meow <abodkaramma77@gmail.com>
2025-01-24 11:51:17 +0200

    there was some mistakes i solved them

--- back-end/createAccount.js
+++ back-end/createAccount.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
-
+const multer = require("multer");
+express=require("express")
+app=express()
 app.use(express.json());
 const cors = require("cors");
 
@@ -20,9 +20,18 @@ const usersSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  img: {
+    type: String,
+    required: false,
+  },
+
   string: {
     type: String,
     required: true,
+  },
+  userName: {
+    type: String,
+    required: false,
   },
 });
 const m = mongoose.model("Meo", usersSchema);
@@ -36,7 +45,7 @@ app.post("/signu", async (req, res) => {
     return;
   }
 
-  CreateAndSaveTheData(req.body)
+  createTheDataAndSaveIt(req.body)
     .then((string) => {
       res.json({ action: "created", string: string });
     })
@@ -45,14 +54,17 @@ app.post("/signu", async (req, res) => {
       console.log(e);
     });
 });
-async function CreateAndSaveTheData(data) {
+async function createTheDataAndSaveIt(data) {
   let string = require("crypto").randomBytes(64).toString("hex");
   data.string = string;
-  console.log(data);
+  data.img = "";
+  data.userName = data.name;
+  console.log(data.img);
   const newAccount = new m(data);
   await newAccount.save();
   return string;
 }
+
 app.post("/check", (req, res) => {
   console.log("checking");
   let find = m.find({ string: req.body });
@@ -64,8 +76,16 @@ app.post("/check", (req, res) => {
 });
 app.post("/signin", (req, res) => {
   check(req.body)
-    .then(() => res.json({ action: "signin" }))
+    .then((r) => {
+      if (r.status === "error") {
+        return res.json({ action: "error" });
+      }
+      console.log(r);
+
+      res.json({ action: "signin", string: r.string });
+    })
     .catch((e) => {
+      console.log("not in the then");
       console.log("error", e);
       res.json({ action: "Error" });
     });
@@ -74,11 +94,10 @@ app.post("/signin", (req, res) => {
 async function check(data) {
   const account = await m.find({ ...data });
   if (account.length === 0) {
-    console.log({ success: false });
-    return;
+    return { status: "error" };
   }
-
-  console.log({ success: true });
+  console.log(account);
+  return { status: "signin", string: account[0].string };
 }
 // ----------------------------------------------------------------------------------------------------------------------------------------
 
@@ -87,11 +106,15 @@ const messagesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-
+  name: {
+    type: String,
+    required: true,
+  },
   time: {
     type: Date,
     required: true,
   },
+
   string: {
     type: String,
     required: true,
@@ -110,6 +133,7 @@ app.post("/messages", async (req, res) => {
   let obj = {
     msgValue: req.body.value,
     time: time,
+    name: req.body.name,
     string: req.body.string,
   };
   const message = new msg(obj);
@@ -117,18 +141,49 @@ app.post("/messages", async (req, res) => {
   res.json({ string: req.body.string, time });
 });
 app.post("/data", async (req, res) => {
-  let findAllMessages = await msg.find().limit(20);
+  let arr = [];
+  let findAllMessages = await msg.find().limit(100);
+  console.log(req.body.string);
+  for (const msg of findAllMessages) {
+    let findImg = await m.findOne({ string: JSON.parse(msg.string) });
 
-  res.json({ messages: findAllMessages, string: req.body.string });
+    msg.img = findImg.img;
+
+    arr.push({ ...msg.toJSON(), img: findImg.img });
+  }
+  res.json({
+    messages: arr,
+    string: req.body.string,
+  });
 });
 app.post("/chocks", async (req, res) => {
-
   let newMessages = await msg.find({ time: { $gt: req.body.time } });
   if (!newMessages) {
-    return res.json({ new: "" ,success:false});
+    return res.json({ new: "", success: false });
   }
-  res.json({ new: newMessages,success:true });
+  res.json({ new: newMessages, success: true });
 });
+//------------------------------------------------------------------------------------------------------------------------------
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post("/files", upload.single("file"), async (req, res) => {
+  console.log(req.file.filename, "youtube.cm");
+  let u = await m.updateOne(
+    { string: req.body.string },
+    { img: req.file.path }
+  );
+  console.log(u, req.body.string);
+  res.json({ message: "File uploaded successfully!" });
+});
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.listen(2000, () => {
   console.log("2000");
 });
